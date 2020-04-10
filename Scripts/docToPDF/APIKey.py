7d533e9f39199a9b00509b1c49c51fd2c6ff8ce1
DivyPatel9881 <divy9881@gmail.com>
2020-04-10 13:21:12 +0530

    DOC to PDF converter.

--- Scripts/docToPDF/APIKey.py
+++ Scripts/docToPDF/APIKey.py
@@ -1,11 +1,16 @@
 import os
 
+env_var_name = 'CONVERT_API_KEY'
+
 def set_api_key(api_key):
-    os.environ['CONVERT_API_KEY'] = api_key
+    os.environ[env_var_name] = api_key
     print("Environment variable has been set for your API key.")
 
 def get_api_key():
-    return os.getenv('CONVERT_API_KEY')
+    return os.getenv(env_var_name)
+
+def clear_api_key():
+    os.environ.pop(env_var_name)
 
 def is_api_key_set():
     api_key = get_api_key()
--- Scripts/docToPDF/docToPDF.py
+++ Scripts/docToPDF/docToPDF.py
@@ -1,6 +1,7 @@
 import convertapi
+from pathlib import Path
 
-from APIKey import *
+from .APIKey import *
 
 if is_api_key_set():
     convertapi.api_secret = get_api_key()
@@ -12,3 +13,15 @@ else:
 
 print("Found your API endpoint.")
 
+input_folder = Path('/Resume-Reader/resumes_in_doc')
+output_folder = Path('/Resume-Reader/resumes_in_pdf')
+
+def doc_to_pdf(input_file_name, output_file_name):
+    input_file_path = input_folder / input_file_name
+    output_file_path = output_folder / output_file_name
+
+    print('Converting ' + input_file_name + ' -> ' + output_file_name + ' .....' )
+    result = convertapi.convert('pdf', { 'File': str(input_file_path) })
+    
+    result.file.save(str(output_file_path))
+    print('PDF file is ready.')
